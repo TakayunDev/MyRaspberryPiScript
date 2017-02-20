02cd34eff21fcc1b15dbbbc2e0e131655238467a
takayun <takkali@hotmail.co.jp>
2017-02-20 11:26:54 +0000

    add daemon and fix mikusay

---
+++ Daemon/daemon_setup.sh
@@ -0,0 +1,2 @@
+sudo cp ./goto-daemon /etc/init.d/
+sudo chmod 0755 /etc/init.d/goto-daemon
--- Voice/mikusay.sh
+++ Voice/mikusay.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 TMP=/tmp/jsay_$$.wav
 
+#HTS=/usr/share/hts-voice/mei/mei_normal.htsvoice
+#HTS=/usr/share/hts-voice/miku/miku_type_β.htsvoice
+HTS=/usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice
+
 from_pip() {
 cat - | awk '{print $1}' | xargs -t -n 1 -Imsg ./jsay.sh 'msg'
 exit
 }
 
 from_arg() {
-  echo "$1" | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/miku/miku_type_β.htsvoice -ow $TMP
+  echo "$1" | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m $HTS -ow $TMP
   aplay --quiet $TMP
   rm -f $TMP
   exit
